a8ee304191bd59bea4ab719174bec04f7e2631aa
NuvaGit <jack.neilan@ucdconnect.ie>
2025-03-27 00:52:44 +0000

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/gym-booking-frontend">
+    <Router basename="/">
       <div className="app-container">
         <Navbar />
         
